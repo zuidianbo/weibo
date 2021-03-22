c105e657ddf28809986fa161ec83d77610690435
HYH <h2005yh@126.com>
2021-03-22 11:15:07 +0000

    HYH 静态文件浏览器缓存问题

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <title>@yield('title', 'Weibo App') -HYH 测试</title>
-    <link rel="stylesheet" href="/css/app.css">
+    {{--<link rel="stylesheet" href="/css/app.css">--}}
+
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
 
